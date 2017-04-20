2845a1458ce845e82bd3818001beb06ecf510f76
Jordan Hudgens <jordan@devcamp.com>
2017-04-20 22:16:15 +0000

    added edit and update actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,7 +9,6 @@ class PortfoliosController < ApplicationController
   
   def create
      @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
@@ -18,4 +17,20 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  
 end
