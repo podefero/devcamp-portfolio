f53b082b61631e66ab00ebe941f65ac3dab12e0a
Jordan Hudgens <jordan@devcamp.com>
2017-05-04 19:14:46 +0000

    integrated a toggle switch for blogs on the draft and published status.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -44,10 +44,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -60,6 +58,11 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: 'Blog was successfully removed.' }
       format.json { head :no_content }
     end
+  end
+  
+  def toggle_status
+   @blog.published? ? @blog.draft! : @blog.published!
+    redirect_to blogs_url
   end
 
   private
