7f80183d0ed7049247d1b1c2585238ed5be72a90
Jordan Hudgens <jordan@devcamp.com>
2017-05-19 22:32:25 +0000

    implemented default values for portfolios images.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
   #same as the above
   scope :ruby_on_rails_portfolio, -> { where(subtitle: "Ruby on Rails") }
+  
+  after_initialize :set_defaults 
+  
+  def set_defaults
+    main_image ||= "https://placeholdit.imgix.net/~text?txtsize=33&txt=600%C3%97400&w=600&h=400"
+    thumb_image ||= "https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97200&w=350&h=200"
+  end
 end
