e7d1104b9add48dc5b405a6fb15f3b960851a60d
Jordan Hudgens <jordan@devcamp.com>
2017-10-30 17:27:38 +0000

    Refactored application in include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :set_source
   
   def set_source 
-    session[:source] = params[:q]
+    session[:source] = params[:q] if params[:q]
   end
 end
